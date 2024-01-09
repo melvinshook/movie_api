99714092b0c6a1d8bd85a7c5035d77456d5769dc
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-01-09 15:10:09 -0500

    added remote access

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const { check, validationResult } = require('express-validator');
 
 
 
-mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongodb://localhost:27017/cfDB
 app.use(bodyParser.json());
 
 
