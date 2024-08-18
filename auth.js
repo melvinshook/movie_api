854960d184c82693d8bba7f93a1e65f79977619f
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-08-17 23:03:24 -0400

    Refactor authentication module to ES modules and improve error handling

--- auth.js
+++ auth.js
@@ -1,34 +1,33 @@
-const jwtSecret = "your_jwt_secret"; // This has to be the same key used in the JWTStrategy
+import jwt from 'jsonwebtoken';
+import passport from 'passport';
+import './passport.js';
 
-const jwt = require("jsonwebtoken"),
-  passport = require("passport");
+const jwtSecret = process.env.JWT_SECRET || "your_jwt_secret"; 
 
-require("./passport"); // Your local passport file
-
-let generateJWTToken = (user) => {
+const generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.userName, // This is the username you’re encoding in the JWT
-    expiresIn: "7d", // This specifies that the token will expire in 7 days
-    algorithm: "HS256", // This is the algorithm used to “sign” or encode the values of the JWT
+    subject: user.userName,
+    expiresIn: "7d", 
+    algorithm: "HS256",
   });
 };
 
-/* POST login. */
-module.exports = (router) => {
-  router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
+/* POST login route */
+export const login = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
       console.log({ error, user });
       if (error || !user) {
         return res.status(400).json({
-          message: "Something is not right",
+          message: 'Something is not right',
           user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
         if (error) {
-          res.send(error);
+          return res.status(500).send(error.message);
         }
-        let token = generateJWTToken(user.toJSON());
+        const token = generateJWTToken(user.toJSON());
         return res.json({ user, token });
       });
     })(req, res);
