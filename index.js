8dd202d557541ea4248f914ff418748fee966d58
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-08-17 21:49:55 -0400

    updates

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const Users = Models.User;
 
 const { check, validationResult } = require("express-validator");
 
-/* mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}); */
-mongoose.connect(process.env.CONNECTION_URI, {
+mongoose.connect("mongodb://localhost:27017/myflixdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+/* mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); */
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
